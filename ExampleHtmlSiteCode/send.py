67277ba44edf26aa62abcf07142997de4cd2a3da
Mr. Paul <fiberphoton@gmail.com>
2013-07-31 08:35:46 -0400

    adding a basic send file using requests

---
+++ ExampleHtmlSiteCode/send.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import requests
+
+request_url = r'http://sample.whataremindsfor.com:26900/apiv1/recognize/'
+appkey = '59c000476cf2a00b9f5555585c177276d8a4d75f'
+appsecret = '01d3cb9455722c2f6760e11f14a05711796aaaaa'
+conkey = '09326eeeee2cad2ae9f4c0fd5d4c7b71db2b0e48'
+consecret = '7092379b31cd621c2c1b574e47ec956733faaaaa'
+locale = 'en-US'
+ttsSpeakerType = 'text'
+
+payload = { 'appkey':appkey,
+            'appsecret': appsecret,
+            'conkey': conkey,
+            'consecret' : consecret,
+            'locale' : locale,
+            'ttsSpeakerType': ttsSpeakerType}
+
+r = requests.post(request_url, data=payload)
+
+print r.text
