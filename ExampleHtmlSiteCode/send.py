865535f205deb80bfa9003bb87f68bd92743a4b5
Mr. Paul <fiberphoton@gmail.com>
2013-08-09 23:07:06 -0400

    Moved Some things
    We moved and tried to Break this file send.py

--- ExampleHtmlSiteCode/send.py
+++ ExampleHtmlSiteCode/send.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#Paul Turner
+# Paul Turner
 # send.py file to push wav to API to return JSON
 
 import requests
@@ -9,16 +9,20 @@ appkey = '59c000476cf2a00b9f5555585c177276d8a4d75f'
 appsecret = '01d3cb9455722c2f6760e11f14a05711796aaaaa'
 conkey = '09326eeeee2cad2ae9f4c0fd5d4c7b71db2b0e48'
 consecret = '7092379b31cd621c2c1b574e47ec956733faaaaa'
+# locale = ''
 locale = 'en-US'
-ttsSpeakerType = 'text'
+ttsSpeakerType = 'usenglishfemale'
+#wav = '/Users/pturner/Documents/Var/Vognition/Vognition/ExampleHtmlSiteCode/ALARM_1.wav'
+# wav = ''
 
-payload = { 'appkey':appkey,
-            'appsecret': appsecret,
-            'conkey': conkey,
+payload = { 'appkey' : appkey,
+            'appsecret' : appsecret,
+            'conkey' : conkey,
             'consecret' : consecret,
             'locale' : locale,
-            'ttsSpeakerType': ttsSpeakerType
-	    'ALARM_1.wav': wav}
+            'ttsSpeakerType' : ttsSpeakerType }
+
+print payload
 
 r = requests.post(request_url, data=payload)
 
