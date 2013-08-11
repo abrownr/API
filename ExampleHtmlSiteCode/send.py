0dad84b299b3ca2437d1bdd0fc45211c0b4a2c49
Mr. Paul <fiberphoton@gmail.com>
2013-08-11 09:30:27 -0400

    Expanding calls within Request
    added  a payload change and printing other Objects calls to r.(call)

--- ExampleHtmlSiteCode/send.py
+++ ExampleHtmlSiteCode/send.py
@@ -4,6 +4,9 @@
 
 import requests
 
+headers = {'content-type': 'multipart/mixed',
+            'boundary': 'gc0p4Jq0M2Yt08jU534c0p'}
+
 request_url = r'http://sample.whataremindsfor.com:26900/apiv1/recognize/'
 appkey = '59c000476cf2a00b9f5555585c177276d8a4d75f'
 appsecret = '01d3cb9455722c2f6760e11f14a05711796aaaaa'
@@ -12,18 +15,19 @@ consecret = '7092379b31cd621c2c1b574e47ec956733faaaaa'
 # locale = ''
 locale = 'en-US'
 ttsSpeakerType = 'usenglishfemale'
-#wav = '/Users/pturner/Documents/Var/Vognition/Vognition/ExampleHtmlSiteCode/ALARM_1.wav'
+wav = '/Users/pturner/Documents/Var/Vognition/Vognition/ExampleHtmlSiteCode/ALARM_1.wav'
 # wav = ''
 
-payload = { 'appkey' : appkey,
+payload = { '59c000476cf2a00b9f5555585c177276d8a4d75f' : appkey,
             'appsecret' : appsecret,
             'conkey' : conkey,
             'consecret' : consecret,
             'locale' : locale,
-            'ttsSpeakerType' : ttsSpeakerType }
+            'ttsSpeakerType' : ttsSpeakerType,
+            'wav' : wav }
 
 print payload
 
-r = requests.post(request_url, data=payload)
+r = requests.post(request_url, data=payload, headers=headers)
 
 print r.text
